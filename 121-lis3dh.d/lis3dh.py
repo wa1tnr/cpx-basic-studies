430cb31032ffe28bf60fa81b9096df9d3f2e5514
wa1tnr <wa1tnr@users.noreply.github.com>
2017-12-14 20:19:22 +0000

    modified:   lis3dh.py

--- 121-lis3dh.d/lis3dh.py
+++ 121-lis3dh.d/lis3dh.py
@@ -29,21 +29,43 @@ while True:
 # z of 1.0.  Flipped over onto the component side (now
 # face-down) and it's x=0 y=0 z=-1.0.
 
+    x = 100 * x
+    y = 100 * y
+    z = 100 * z
+
     print('x = {}G, y = {}G, z = {}G'.format(x / 9.806, y / 9.806, z / 9.806))
 
 
 # x axis goes thru USB connector and its wire
 #   print('x = {}G'.format(x / 9.806))
 
+# The device can be rotated on an axis passing through both
+# pb switches, without disturbing the x axis.
+
+# Or, equivalently,
+# the device can be rotated on the y axis, without disturbing
+# the x axis (and vice-versa).
 
 # y axis goes thru both pb switches
 #   print('y = {}G'.format(y / 9.806))
 
+# The device can be rotated on an axis passing through the USB
+# and battery connection, without disturbing the y axis.
 
 # z axis is heads or tails (if z is positive: HEADS; if negative: TAILS).
 # z nulls when the coin rolls on edge, throughout that roll.
 #   print('z = {}G'.format(z / 9.806))
 
+# The device can be rotated on an axis passing through the plane
+# of its circuit board, without disturbing the z axis.
+
+# It is helpful to think of an imaginary bubble level (in a long
+# cylinder shape) passing through each axes, and how by rotating
+# the device on the axis of that cylinder, one of the three
+# readings (x, y or z) remains constant throughout that rotation.
+
+# That is to say, the imaginary bubble stays centered in the
+# level, even when rolled.
 
     # Small delay to keep things responsive but give time for interrupt processing.
     time.sleep(0.1)
