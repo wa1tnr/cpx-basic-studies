91545d756e90dfcc5f95fcab2d108252ac6b4a62
wa1tnr <wa1tnr@users.noreply.github.com>
2018-02-13 23:18:43 +0000

    modified:   main.py

--- 102-neopixel.d/main.py
+++ 102-neopixel.d/main.py
@@ -1,5 +1,6 @@
 # Adafruit CircuitPython 2.2.0
 # Adafruit CircuitPlayground Express
+
 import board ; import neopixel; import time
 pixels = neopixel.NeoPixel(board.NEOPIXEL, 10, brightness=.2)
 pixels.fill((0,0,0))
